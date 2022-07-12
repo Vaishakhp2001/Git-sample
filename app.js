d872e298e6877787462199e3ffbdc943f9168fbf
Vaishakh <vaishakhp11@gmail.com>
2022-07-12 17:09:22 +0530

    edit file

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+app.get('/', function (req, res) {
+   res.send('Hello World');
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
    
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(3000);
-
-// Console will print the message
-console.log('Server running ');
+   console.log("Example app listening at http://%s:%s", host, port)
+})
