ea5e022e0da719d7501cda0fb4b66b4733d68aab
Vaishakh <vaishakhp11@gmail.com>
2022-07-30 11:45:46 +0530

    node app

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-})
+app.get('/', (req, res) => res.send({status: "I'm alive!"}));
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
